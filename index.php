ea3bb887605b46d04645ff9644f2fbec1c5bfb06
Sergei <serjoga-zavitaev@rambler.ru>
2012-04-02 22:07:07 +0300

    index file add

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<!--  -->
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title>Документ без названия</title>
+<link href="style/style.css" rel="stylesheet" type="text/css">
+<link href="style/styleMenu.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<!--  wrapper -->
+<div id = "wrapper">
+		<div id = "header">
+		</div>
+        <div id="menu">
+		<ul>
+			<li><a href="#" class="current"><span>home</span></a></li>
+			<li><a href="#"><span>about us</span></a></li>
+			<li><a href="#"><span>services</span></a></li>
+			<li><a href="#"><span>solutions</span></a></li>
+			<li><a href="#"><span>contact us</span></a></li>								
+		</ul>
+		</div>
+		<div id="contentWrapper">
+        	<div id="leftside">
+			           
+        	</div>
+        	<div id="rightside">
+        	</div>
+        	<div id="content">
+
+		</div>
+        </div>
+		<div id="clear">
+            </div>
+        <div id = "footer">
+            	<div id="links">
+                	<ul id="link_list"> 
+                    	 <li><a href="#"><img class="footer_img" src="images/img/google.png" alt="google"/> </a></li> 
+           	 			 <li><a href="#"> <img class="footer_img" src="images/img/twitter.png" alt="twitter"/> </a></li>
+           	 			 <li><a href="#"> <img class="footer_img" src="images/img/vkontakte.png" alt="vkontakte"/> </a></li>
+       	 				 <li><a href="#"> <img class="footer_img" src="images/img/youtube.png" alt="youtube"/> </a></li>
+                    </ul>     
+                 </div>
+        </div>
+</div>
+
+
+
+</body>
+</html>
